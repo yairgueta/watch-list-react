c56959eabd76a2f530569320d5f28a765994f8e7
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-06-24 17:26:44 +0300

    inits

---
+++ src/components/watchItem.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class WatchItem extends Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+export default WatchItem;
