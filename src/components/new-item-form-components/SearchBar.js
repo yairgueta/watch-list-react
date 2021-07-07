4e7fac6250880c97b901597620eaa8752baa3e61
yairgueta <Yair.gueta@mail.huji.ac.il>
2021-07-07 21:04:36 +0300

    working on search bar (maybe using someone elses component?)

--- src/components/new-item-form-components/SearchBar.js
+++ src/components/new-item-form-components/SearchBar.js
@@ -1,10 +1,14 @@
 import React, {useState} from 'react';
 import './search-bar.css'
 import {Dropdown} from "react-bootstrap";
+import Autosuggest from 'react-autosuggest';
+
 
 const SearchBar = ({searchKeyword}) => {
     const [searchList, setSearchList] = useState([]);
-
+    const [searchBarFocus, setSearchBarFocus] = useState(false);
+    const [listFocus, setListFocus] = useState(false);
+    let nameInput = null;
 
     const handleSearch = async (keyword) => {
         const results = await searchKeyword(keyword);
@@ -27,13 +31,33 @@ const SearchBar = ({searchKeyword}) => {
             <div className="dropdown">
                 <div className='form-floating mb-2'>
                     <input type='search' id='movie-name' className='form-control' placeholder=""
-                           onChange={(e) => handleSearch(e.target.value)}/>
+                           onChange={(e) => handleSearch(e.target.value)}
+                           onFocus={()=>setSearchBarFocus(true)}
+                           onBlur={()=>setSearchBarFocus(false)}
+                    />
                     <label htmlFor='movie-name'>Movie Name</label>
                 </div>
-                <ul className="dropdown-menu show" aria-labelledby="dropdownMenuButton">
+                {(searchBarFocus || listFocus) &&
+                    <ul className="dropdown-menu show" aria-labelledby="dropdownMenuButton"
+                        onFocus={()=>setListFocus(true)}
+                        onBlur={()=>setListFocus(false)}
+                    >
                     {searchList.map((item, index) =>
                         <li><a href="#" className="dropdown-item user-select-none" key={index}>{itemToString(item)}</a></li>)}
-                </ul>
+                    </ul>
+                }
+                <Autosuggest
+                    suggestions={searchList}
+                    onSuggestionsFetchRequested={(v) => {
+                        console.log("suggestion fetch",v)}}
+                    // onSuggestionsClearRequested={}
+                    getSuggestionValue={(v) => {
+                        console.log("get sugg valu", v)
+                    }}
+                    // renderSuggestion={}
+                    // inputProps={inputProps}
+                />
+
             </div>
         </>
     );
